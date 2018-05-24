8673c90919f395dba1279656dc14d2b62056b3a5
Mohamed Ahmed <error17191@gmail.com>
2018-05-24 12:45:03 +0200

    First Commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+Route::prefix('auth')->group(function (){
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+});
